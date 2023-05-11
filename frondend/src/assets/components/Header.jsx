bb63887c96442b572bd3e5cf442f906887b5abf2
Tshepo2022 <tshepotebe@gmail.com>
2023-05-11 12:58:17 +0200

    added function header

--- frondend/src/assets/components/Header.jsx
+++ frondend/src/assets/components/Header.jsx
@@ -6,20 +6,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Header() {
   return (
-    <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand href="#home">Vintage Cars</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/login">Login</Nav.Link>
-            <Nav.Link href="/register">Register</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+  //   <Navbar bg="light" expand="lg">
+  //     <Container>
+  //       <Navbar.Brand href="#home">Vintage Cars</Navbar.Brand>
+  //       <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  //       <Navbar.Collapse id="basic-navbar-nav">
+  //         <Nav className="me-auto">
+  //           <Nav.Link href="/">Home</Nav.Link>
+  //           <Nav.Link href="/login">Login</Nav.Link>
+  //           <Nav.Link href="/register">Register</Nav.Link>
+  //         </Nav>
+  //       </Navbar.Collapse>
+  //     </Container>
+  //   </Navbar>
+  // );
 }
 
 export default Header;
