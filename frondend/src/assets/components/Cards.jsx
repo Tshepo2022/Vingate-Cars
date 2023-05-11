d52dc99d122b6a8b6f904a4b4aa80ca6948c395b
Tshepo2022 <tshepotebe@gmail.com>
2023-05-11 13:02:02 +0200

    added import Cards

--- frondend/src/assets/components/Cards.jsx
+++ frondend/src/assets/components/Cards.jsx
@@ -4,28 +4,28 @@ import './Cards.css'
 
 function Cards(props) {
     return (
-        <div>
-            <div class="grid-container">
-                {
-                    props.item.map(products => {
+    //     <div>
+    //         <div class="grid-container">
+    //             {
+    //                 props.item.map(products => {
 
-                        return (
-                            <div className='card'>
-                                <img src={products.image} className="card-img-top" alt='...' />
-                                <div className="card-body">
-                                    <h5 className="card-title">{products.name}</h5>
-                                    <h3>{products.price}</h3>
-                                    <a href="#" className="btn btn-primary">Add Cart</a>
-                                </div>
-                            </div>
+    //                     return (
+    //                         <div className='card'>
+    //                             <img src={products.image} className="card-img-top" alt='...' />
+    //                             <div className="card-body">
+    //                                 <h5 className="card-title">{products.name}</h5>
+    //                                 <h3>{products.price}</h3>
+    //                                 <a href="#" className="btn btn-primary">Add Cart</a>
+    //                             </div>
+    //                         </div>
 
-                        )
-                    })
-                }
+    //                     )
+    //                 })
+    //             }
 
-            </div>
-        </div>
-    );
+    //         </div>
+    //     </div>
+    // );
 }
 
 export default Cards;
